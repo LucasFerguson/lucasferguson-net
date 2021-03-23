3c3cf5b4036900298585de970c7f723c9a7165a4
Lucas <lucasferguson16@gmail.com>
2021-03-23 12:17:29 -0500

    home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit } from '@angular/core';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
-  constructor() { }
+	@ViewChild("myDiv") divView: ElementRef | undefined;
+	@ViewChild("myCanvas") canvas: ElementRef | undefined;
 
-  ngOnInit(): void {
-  }
+	ngAfterViewInit() {
 
+		if (this.canvas) {
+			var ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext("2d");
+
+
+			var e: CanvasRenderingContext2D = this.canvas.nativeElement;
+			ctx.canvas.width = window.innerWidth;
+			ctx.canvas.height = window.innerHeight;
+			ctx.beginPath();
+			ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+			ctx.stroke();
+			console.log();
+
+		}
+	}
 }
