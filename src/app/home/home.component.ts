3e055f43090143c89e6b85dab1e421553f198800
Lucas <lucasferguson16@gmail.com>
2021-04-18 13:44:14 -0500

    4/18/2021

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,22 +10,22 @@ import { AfterViewInit } from '@angular/core';
 })
 export class HomeComponent implements AfterViewInit {
 
-	@ViewChild("myDiv") divView: ElementRef | undefined;
-	@ViewChild("myCanvas") canvas: ElementRef | undefined;
+	@ViewChild("myCanvas") canvas: ElementRef<HTMLCanvasElement> | undefined;
 
 	ngAfterViewInit() {
 
-		if (this.canvas) {
+		if (!this.canvas) {
+
 			var ctx: CanvasRenderingContext2D = this.canvas.nativeElement.getContext("2d");
 
-
-			var e: CanvasRenderingContext2D = this.canvas.nativeElement;
 			ctx.canvas.width = window.innerWidth;
 			ctx.canvas.height = window.innerHeight;
 			ctx.beginPath();
 			ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 			ctx.stroke();
 			console.log();
+
+
 
 		}
 	}
